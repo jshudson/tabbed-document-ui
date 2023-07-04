2baf10a21d9199d73a3346075eeefa7ff000ce62
Jeremy Hudson <jshudson.ch@gmail.com>
2023-07-04 15:09:03 -0400

    events

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-const parent = document.getElementById("parent");
+//@ts-check
+const containerDiv = document.getElementById("parent");
 
 const createElementWithClass = (type, classList) => {
   const newElement = document.createElement(type);
@@ -13,12 +14,36 @@ const splitContainer = (parent) => {
   const firstSplit = container.appendChild(
     createElementWithClass("div", "resizeable-content")
   );
+  /**@type {HTMLElement} */
   const divider = container.appendChild(
     createElementWithClass("div", "resizeable-handle")
   );
   const secondSplit = container.appendChild(
     createElementWithClass("div", "resizeable-content")
   );
+  const mouseDown = (e) => {
+    /**
+     *
+     * @param {MouseEvent} e
+     */
+    const mouseMove = (e) => {
+      container.setAttribute('grid-template-columns', `${e.offsetX}px 14px auto`)
+      console.log(firstSplit)
+    };
+    container.addEventListener("mousemove", mouseMove);
+    container.addEventListener(
+      "mouseup",
+      (e) => {
+        container.removeEventListener("mousemove", mouseMove);
+      },
+      { once: true }
+    );
+    const click = { x: e.clientX, y: e.clientY };
+    const loc = { x: divider.offsetLeft, y: divider.offsetTop };
+    console.log(click, loc);
+  };
+  console.log(divider);
+  divider.addEventListener("mousedown", mouseDown);
 };
 
-splitContainer(parent);
+splitContainer(containerDiv);
